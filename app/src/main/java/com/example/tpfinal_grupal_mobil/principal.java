a01bf8b29b03b6443c5e669acc377a83597ac340
farpires <55299077+farpires@users.noreply.github.com>
2019-11-02 20:43:37 -0300

    .

---
+++ app/src/main/java/com/example/tpfinal_grupal_mobil/principal.java
@@ -0,0 +1,18 @@
+package com.example.tpfinal_grupal_mobil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal);
+
+
+
+    }
+}
